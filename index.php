94ed0d7b597051a6dd20243fb8128d9bb250835c
stani1940 <stanislav1940@abv.bg>
2020-02-02 22:59:19 +0200

    Add clockHandAngle function

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
-
-
-</body>
-</html>
-
 <?php 
 
 include('header.php');
 
-
+function clockHandAngle($time) {
+    $time = explode(':', $time);
+    $hours = $time[0];
+    $minutes = $time[1];
+    $seconds = $time[2];
+    
+if($hours > 12) {
+        $hours -= 12;
+    }
+    // Formula for finding angle
+    $angle = abs(30 * $hours + 0.5 * $minutes + ($seconds / 120) - (6 * $minutes + 0.1 * $seconds));
+    // Return the smaller angle
+    return min($angle, 360 - $angle);
+}
+echo clockHandAngle("12:00:00")."<br>";
+echo clockHandAngle("3:00:00");
+include('footer.php');
 
  ?>
